2190b60a7fa00106be49fb6fc8c91b35ed7d1df0
jithin <jithin@jithin-WIV68B55-S-0017>
2016-01-06 17:38:33 +0530

    member entity

---
+++ IPL/src/main/java/com/inapp/ipl/entities/Member.java
@@ -0,0 +1,75 @@
+package com.inapp.ipl.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="members")
+public class Member {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@OneToOne
+	@JoinColumn(name = "teamId")
+	private Team team;
+	
+	@Column(name="isCaptain")
+	private boolean isCaptain;
+	
+	@Column(name="imageUrl")
+	private String imageUrl;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+
+	public boolean isCaptain() {
+		return isCaptain;
+	}
+
+	public void setCaptain(boolean isCaptain) {
+		this.isCaptain = isCaptain;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
+
+}
