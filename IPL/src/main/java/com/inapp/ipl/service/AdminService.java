b78538327edf93a6fdc20b8cae8e016f850009e2
jude <jude@in.inapp.com>
2016-01-06 09:51:41 +0530

    entity removed

--- IPL/src/main/java/com/inapp/ipl/service/AdminService.java
+++ IPL/src/main/java/com/inapp/ipl/service/AdminService.java
@@ -1,16 +1,5 @@
 package com.inapp.ipl.service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-
-import com.inapp.ipl.entity.Department;
-import com.inapp.ipl.entity.Doctor;
-import com.inapp.ipl.entity.Hospital;
 
 public interface AdminService {
 
