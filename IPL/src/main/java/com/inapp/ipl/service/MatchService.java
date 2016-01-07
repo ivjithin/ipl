b0aa1ad4e7b99c5790ed782fa3acf004ddd303be
jude <jude@in.inapp.com>
2016-01-07 13:41:52 +0530

    match deatils

--- IPL/src/main/java/com/inapp/ipl/service/MatchService.java
+++ IPL/src/main/java/com/inapp/ipl/service/MatchService.java
@@ -1,14 +1,39 @@
 package com.inapp.ipl.service;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.inapp.ipl.dao.MatchDAO;
 import com.inapp.ipl.dto.Match;
+import com.inapp.ipl.entities.BattingDetails;
+import com.inapp.ipl.entities.BowlingDetails;
 
 @Service
 public class MatchService {
 
+	@Autowired
+	private MatchDAO matchDAO;
+
 	public boolean saveMatchDetails(Match match) {
-		// TODO Auto-generated method stub
+		int matchId = matchDAO.saveSummary(match.getSummary());
+		BattingDetails battingDetails1 = match.getFirstInnings()
+				.getBattingDetails();
+		BowlingDetails bowlingDetails1 = match.getFirstInnings()
+				.getBowlingDetails();
+		BattingDetails battingDetails2 = match.getSecondInnings()
+				.getBattingDetails();
+		BowlingDetails bowlingDetails2 = match.getSecondInnings()
+				.getBowlingDetails();
+		battingDetails1.setMatchId(matchId);
+		battingDetails2.setMatchId(matchId);
+		bowlingDetails1.setMatchId(matchId);
+		bowlingDetails2.setMatchId(matchId);
+		matchDAO.saveBattingDetails(Arrays.asList(battingDetails1,
+				battingDetails2));
+		matchDAO.saveBowlingDetails(Arrays.asList(bowlingDetails1,
+				bowlingDetails2));
 		return true;
 	}
 
