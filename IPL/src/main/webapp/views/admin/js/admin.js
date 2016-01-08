71fa08a96302c8315d9b33254917513099647702
ciby <ciby@inapp.com>
2016-01-08 10:24:26 +0530

    admin save

--- IPL/src/main/webapp/views/admin/js/admin.js
+++ IPL/src/main/webapp/views/admin/js/admin.js
@@ -5,7 +5,7 @@ var contextPath="";
 	
 	var service=(function(){
 		
-		var loadBattingTables=function(battingDtls,innType){
+		var loadBattingTables=function(battingDtls,innType,innName){
 			var battingHtml="",
 			    bowlingHtml="";			
 			
@@ -14,41 +14,47 @@ var contextPath="";
 				battingHtml+="<tr data-memberid='"+eachObj.id+"' data-teamid='"+eachObj.team.id+"'>";
 				battingHtml+="<td scope='row'>"+(i*1+1)+"</td>";
 				battingHtml+="<td><input type='text' name='playername' class='col-lg-12' value="+eachObj.name+" readOnly ></td>";
-				battingHtml+="<td><input type='text' name='runs'  class='col-lg-10'/></td>";
-				battingHtml+="<td><input type='text' name='balls' class='col-lg-10'/></td>";
-				battingHtml+="<td><input type='text' name='four' class='col-lg-10'/></td>";
-				battingHtml+="<td><input type='text' name='six' class='col-lg-10'/></td>";
+				battingHtml+="<td><input type='text' name='runs'  class='col-lg-10' value='0'/></td>";
+				battingHtml+="<td><input type='text' name='balls' class='col-lg-10' value='0'/></td>";
+				battingHtml+="<td><input type='text' name='four' class='col-lg-10' value='0'/></td>";
+				battingHtml+="<td><input type='text' name='six' class='col-lg-10' value='0'/></td>";
 				battingHtml+="<td><input type='checkbox' name='wicketStatus'></td>";
 				battingHtml+="<td><input type='checkbox'  name='isBatted'></td>";
 				battingHtml+="<td><input type='text' name='wicketDescription' class='col-lg-12'/></td>";
-				battingHtml+="<td><a href='javascript:void(0)' class='removeCl'>remove</a></td>";
+				battingHtml+="<td><a href='javascript:void(0)' class='removeCl glyphicon glyphicon-remove'></a></td>";
 				battingHtml+="</tr>";	
 								
 				bowlingHtml+="<tr  data-memberid='"+eachObj.id+"' data-teamid='"+eachObj.team.id+"'>";
 				bowlingHtml+="<th scope='row'>"+(i*1+1)+"</th>";
 				bowlingHtml+="<td><input type='text' name='playername'  class='col-lg-12' value="+eachObj.name+" readOnly /></td>";
-				bowlingHtml+="<td><input type='text' name='overs'  class='col-lg-10' /></td>";
-				bowlingHtml+="<td><input type='text' name='maiden' class='col-lg-10' /></td>";
-				bowlingHtml+="<td><input type='text' name='dotBalls' class='col-lg-10' /></td>";
-				bowlingHtml+="<td><input type='text' name='runsGiven' class='col-lg-10' /></td>";
-				bowlingHtml+="<td><input type='text' name='wickets' class='col-lg-10' /></td>";
-				bowlingHtml+="<td><input type='text' name='economy' class='col-lg-10' /></td>";
-				bowlingHtml+="<td><input type='text' name='catches' class='col-lg-10' /></td>";
-				bowlingHtml+="<td><input type='text' name='runout'  class='col-lg-10' /></td>";
-				bowlingHtml+="<td><a href='javascript:void(0)' class='removeCl'>remove</a></td>";
+				bowlingHtml+="<td><input type='text' name='overs'  class='col-lg-10' value='0' /></td>";
+				bowlingHtml+="<td><input type='text' name='maiden' class='col-lg-10' value='0'/></td>";
+				bowlingHtml+="<td><input type='text' name='dotBalls' class='col-lg-10' value='0' /></td>";
+				bowlingHtml+="<td><input type='text' name='runsGiven' class='col-lg-10' value='0' /></td>";
+				bowlingHtml+="<td><input type='text' name='wickets' class='col-lg-10' value='0'/></td>";
+				bowlingHtml+="<td><input type='text' name='economy' class='col-lg-10' value='0' /></td>";
+				bowlingHtml+="<td><input type='text' name='catches' class='col-lg-10' value='0' /></td>";
+				bowlingHtml+="<td><input type='text' name='runout'  class='col-lg-10' value='0' /></td>";
+				bowlingHtml+="<td><a href='javascript:void(0)' class='removeCl glyphicon glyphicon-remove'></a></td>";
 				bowlingHtml+="</tr>";
 				
 			}
 			if(1===innType){
 				$("#firstInnBattingtable tbody").html(battingHtml);			
 				$("#secondInnBowlingtable tbody").html(bowlingHtml);
+				
+				$("#firstInnBattingTitle").html(" Batting : <b>"+innName+"</b>");
+				$("#secondInnBowlingTitle").html(" Bowling : <b>"+innName+"</b>");
 			}else{
 
 				$("#secondInnBattingtable tbody").html(battingHtml);
 				$("#firstInnBowlingtable tbody").html(bowlingHtml);
+				
+				$("#secondInnBattingTitle").html(" Batting : <b>"+innName+"</b>");
+				$("#firstInnBowlingTitle").html(" Bowling : <b>"+innName+"</b>");
 			}			
 		};
-		var populateBattingDtls=function(teamId,innType){
+		var populateBattingDtls=function(teamId,innType,innName){
 			if(-1!==teamId){				
 				var apiObj={
 						url:contextPath+ "../../team/members/"+teamId,
@@ -56,12 +62,12 @@ var contextPath="";
 					 	contentType:'application/json',			
 				};			
 				IPLCom.ajaxService.invoke(apiObj).done(function(res) {
-					loadBattingTables(res.result,innType);	
+					loadBattingTables(res.result,innType,innName);	
 				}).fail(IPLCom.exceptionHandler.ajaxFailure);				
 			}
 		};
 		
-		var getBattingDetails=function(tableEl){			
+		var getBattingDetails=function(tableEl,innings){			
 			var battingArr=[];
 			$(tableEl+" tbody tr").each(function(){	
 				var battingObj={};
@@ -76,12 +82,13 @@ var contextPath="";
 				battingObj["isBatted"]=$(this).find("input[name=isBatted]").is(':checked')?1:0;
 				battingObj["wicketStatus"]=$(this).find("input[name=wicketStatus]").is(':checked')?1:0;				
 				battingObj["wicketDescription"]=$(this).find("input[name=wicketDescription]").val()*1;
+				battingObj["innings"]=innings;
 				battingArr.push(battingObj);
 			});
 		
 			return battingArr;
 		};
-		var getBowlingDetails=function(tableEl){			
+		var getBowlingDetails=function(tableEl,innings){			
 			var bowlingArr=[];
 			$(tableEl+" tbody tr").each(function(){	
 				var bowlingObj={};
@@ -97,6 +104,7 @@ var contextPath="";
 				bowlingObj["economy"]=$(this).find("input[name=economy]").val()*1;
 				bowlingObj["catches"]=$(this).find("input[name=catches]").val()*1;			
 				bowlingObj["runout"]=$(this).find("input[name=runout]").val()*1;
+				bowlingObj["innings"]=innings;
 				bowlingArr.push(bowlingObj);
 			});
 			return bowlingArr;
@@ -132,10 +140,10 @@ var contextPath="";
 			
 			var firstInnings={},
 				secondInnings={};			
-			firstInnings["battingDetails"]=getBattingDetails("#firstInnBattingtable");
-			firstInnings["bowlingDetails"]=getBowlingDetails("#firstInnBowlingtable");			
-			secondInnings["battingDetails"]=getBattingDetails("#secondInnBattingtable");
-			secondInnings["bowlingDetails"]=getBowlingDetails("#secondInnBowlingtable");			
+			firstInnings["battingDetails"]=getBattingDetails("#firstInnBattingtable",1);
+			firstInnings["bowlingDetails"]=getBowlingDetails("#firstInnBowlingtable",1);			
+			secondInnings["battingDetails"]=getBattingDetails("#secondInnBattingtable",2);
+			secondInnings["bowlingDetails"]=getBowlingDetails("#secondInnBowlingtable",2);			
 			saveData["firstInnings"]=firstInnings;
 			saveData["secondInnings"]=secondInnings;
 			
@@ -147,12 +155,20 @@ var contextPath="";
 				 	contentType:'application/json',	
 				 	data:saveData
 			};
-		
-			IPLCom.ajaxService.invoke(apiObj).done(function(res) {
+			$("select").select2("val", "-1");
+			/*IPLCom.ajaxService.invoke(apiObj).done(function(res) {
 				if(res.result){
 					$(".savedMsg").removeClass("error");
 					$(".savedMsg").addClass("success");							
 					$(".savedMsg").html("Saved Successfully");
+					
+					$("#firstInnBattingtable tbody").html("");			
+					$("#secondInnBowlingtable tbody").html("");
+					$("#secondInnBattingtable tbody").html("");
+					$("#firstInnBowlingtable tbody").html("");
+					$("#winnerTeamResults").val("");
+					$("#matchNo").val("");
+					$("select").select2("val", "-1");
 				}else{
 					$(".savedMsg").removeClass("success");	
 					$(".savedMsg").addClass("error");						
@@ -211,12 +227,14 @@ var contextPath="";
 			_root.el.window.on('resize', _root.windowResizeHandler);
 			
 			$("#firstInnTeamSelect").change(function(){
-				var teamId=$(this).val()*1;
-				service.populateBattingDtls(teamId,1);
+				var teamId=$(this).val()*1,
+				optionText=$(this).find("option:selected").text();
+				service.populateBattingDtls(teamId,1,optionText);
 			});
 			$("#secondInnTeamSelect").change(function(){
-				var teamId=$(this).val()*1;
-				service.populateBattingDtls(teamId,2);
+				var teamId=$(this).val()*1,
+				optionText=$(this).find("option:selected").text();
+				service.populateBattingDtls(teamId,2,optionText);
 			});
 			
 			$("#saveMatch").click(function(){
